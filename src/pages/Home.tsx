79199d65273eedbc2a677315d237200861a49887
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-07-30 22:08:47 +0300

    Add image upload input

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { FlexRow } from '../components/search/layout/alignment/Flex';
 import SearchBlock from '../components/search/SearchBlock';
 import ExternalURL from '../components/UI/ExternalURL';
 import uploadImage from '../data/api/imgur';
@@ -6,17 +7,28 @@ import useClipboard from '../hooks/clipboard';
 
 const Home = () => {
   const pastedData = useClipboard();
+  const [file, setFile] = useState<File>();
   const [imageUrl, setImageUrl] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
+  // Catch a file being pasted
   useEffect(() => {
     if (!(pastedData instanceof File)) {
       return;
     }
+    setFile(pastedData);
+  }, [pastedData]);
+
+  // Upload an image to an image hosting
+  useEffect(() => {
+    if (file === undefined) {
+      return;
+    }
     setIsLoading(true);
     setImageUrl('');
-    uploadImage(pastedData)
+    uploadImage(file)
       .then(
         ({ data, success }) => {
           if (success) {
@@ -32,30 +44,41 @@ const Home = () => {
         },
       )
       .finally(() => setIsLoading(false));
-  }, [pastedData]);
+  }, [file]);
 
-  if (pastedData instanceof File) {
+  if (file === undefined) {
     return (
       <div>
-        {error && (
-          <p>
-            Error: <b>{error}</b>
-          </p>
-        )}
-        {isLoading && <p>Uploading to imgur...</p>}
-        {imageUrl && (
-          <>
-            <p>
-              Your image on imgur: <ExternalURL href={imageUrl}>{imageUrl}</ExternalURL>
-            </p>
-            <SearchBlock imageUrl={imageUrl} />
-          </>
-        )}
+        <p>Just paste an image!</p>
+        <p>or...</p>
+        <FlexRow gap="1em">
+          <input type="file" ref={fileInputRef} />
+          <button type="button" onClick={() => setFile(fileInputRef.current?.files![0])}>
+            Upload and search
+          </button>
+        </FlexRow>
       </div>
     );
   }
 
-  return <p>Paste an image here!</p>;
+  return (
+    <div>
+      {error && (
+        <p>
+          Error: <b>{error}</b>
+        </p>
+      )}
+      {isLoading && <p>Uploading to imgur...</p>}
+      {imageUrl && (
+        <>
+          <p>
+            Your image on imgur: <ExternalURL href={imageUrl}>{imageUrl}</ExternalURL>
+          </p>
+          <SearchBlock imageUrl={imageUrl} />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
