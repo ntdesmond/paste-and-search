9abb2b6e2d493e0d15dd484cb442d6ebf1fb4d70
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-07-30 15:10:48 +0300

    add useClipboard hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,16 @@
-const Home = () => <h1>Hello world!</h1>;
+import useClipboard from '../hooks/clipboard';
+
+const Home = () => {
+  const pastedData = useClipboard();
+
+  // eslint-disable-next-line no-console
+  console.log(pastedData);
+
+  if (pastedData instanceof File) {
+    return <p>You pasted a file of type {pastedData.type}</p>;
+  }
+
+  return <p>Paste an image here!</p>;
+};
 
 export default Home;
