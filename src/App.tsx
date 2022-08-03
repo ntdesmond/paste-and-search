c8c8d252139408cd942fbcf3777b9d500f8cba44
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2022-08-03 13:04:37 +0300

    Remove <Routes/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 
 const App = () => (
   <Layout>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Home />
   </Layout>
 );
 
